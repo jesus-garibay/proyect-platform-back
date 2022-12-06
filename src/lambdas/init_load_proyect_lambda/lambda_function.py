e4728a8b0231f2c97b3c29775c8691364a6ae13b
Ing.Gary <seus4@hotmail.com>
2022-12-06 12:21:47 -0600

    Revision Lambda

--- src/lambdas/init_load_proyect_lambda/lambda_function.py
+++ src/lambdas/init_load_proyect_lambda/lambda_function.py
@@ -1,7 +1,9 @@
 import boto3
 import json
+from core_api.responses import api_response
+from core_utils.utils import get_logger
 
-
+LOGGER = get_logger("")
 def lambda_handler(event, context):
     client = boto3.resource("dynamodb")
     table = client.Table("suscribers")
@@ -12,19 +14,19 @@ def lambda_handler(event, context):
     for suscriber in suscribers:
         suscribers_list += suscriber["name"] + " "
 
-    print("""{'statusCode': 200,
-        'body': json.dumps(suscribers_list),
-        'headers': {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-        }""")
-
-    return {
-        'statusCode': 200,
-        # 'body': json.dumps('Hello from Lambda!')
-        'body': json.dumps(suscribers_list),
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-    }
+    # print("""{'statusCode': 200,
+    #     'body': json.dumps(suscribers_list),
+    #     'headers': {
+    #     'Content-Type': 'application/json',
+    #     'Access-Control-Allow-Origin': '*'
+    #     }""")
+    return api_response(suscribers_list, 200)
+    # return {
+    #     'statusCode': 200,
+    #     # 'body': json.dumps('Hello from Lambda!')
+    #     'body': json.dumps(suscribers_list),
+    #     'headers': {
+    #         'Content-Type': 'application/json',
+    #         'Access-Control-Allow-Origin': '*'
+    #     },
+    # }
--- src/lambdas/init_load_proyect_lambda/test_lambda_function.py
+++ src/lambdas/init_load_proyect_lambda/test_lambda_function.py
@@ -1,18 +1,30 @@
 from unittest import TestCase
-from core_api.utils import get_status_code
 from .lambda_function import lambda_handler
 from core_utils.decorators import ignore_warnings
+from core_api.utils import get_status_code, get_body
+from core_utils.utils import get_logger
+
+LOGGER = get_logger("")
+
 
 class TestLambda(TestCase):
     def setUp(self) -> None:
         self.event = {
-                      "key1": "value1",
-                      "key2": "value2",
-                      "key3": "value3"
-                    }
+            "key1": "value1",
+            "key2": "value2",
+            "key3": "value3"
+        }
 
     @ignore_warnings
     def test_lambda_handler(self):
+        # result = lambda_handler(self.event, None)
+        # status_code = get_status_code(result)
+        # self.assertEqual(200, status_code)
         result = lambda_handler(self.event, None)
+        self.__generic_test_save_get_client_information(result, 200)
+
+    def __generic_test_save_get_client_information(self, result, expected_code):
         status_code = get_status_code(result)
-        self.assertEqual(200, status_code)
+        body = get_body(result)
+        LOGGER.info(body)
+        self.assertEqual(expected_code, status_code)
