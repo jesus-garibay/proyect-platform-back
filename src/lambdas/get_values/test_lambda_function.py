0f932cfee4b7fd789d521da267df05a2e08d56aa
Ing.Gary <seus4@hotmail.com>
2022-12-08 22:02:46 -0600

    Revision Lambda

---
+++ src/lambdas/get_values/test_lambda_function.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from .lambda_function import lambda_handler
+from core_api.utils import get_status_code, get_body
+from core_utils.utils import get_logger
+
+LOGGER = get_logger("get_values")
+
+
+class TestLambda(TestCase):
+    def setUp(self) -> None:
+        self.event = {
+            "headers": {
+                "Authorization": ""
+            }
+        }
+
+
+    def test_lambda_handler(self):
+        result = lambda_handler(self.event, None)
+        self.__generic_test(result, 200)
+
+
+    def __generic_test(self, result, expected_code):
+        status_code = get_status_code(result)
+        body = get_body(result)
+        LOGGER.info(body)
+        self.assertEqual(expected_code, status_code)
