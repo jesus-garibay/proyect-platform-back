9b584a8ff92ff22b8d5839f847cc3c336185f2ce
Ing.Gary <seus4@hotmail.com>
2022-12-11 23:02:52 -0600

    PE-2 Agregar envio de parametro por Header

--- src/lambdas/get_values/test_lambda_function.py
+++ src/lambdas/get_values/test_lambda_function.py
@@ -10,7 +10,7 @@ class TestLambda(TestCase):
     def setUp(self) -> None:
         self.event = {
             "headers": {
-                "valor": "Hello Gary2"
+                "valor": "Hello Gary3"
             }
         }
 
